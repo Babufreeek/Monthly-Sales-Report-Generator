4695734fdbeedfa4454a890e32554eaf342e9bec
Chew Wei Jie <weijie.chew@gmail.com>
2024-01-18 10:47:52 +0800

    Added stylesheet

---
+++ styles.py
@@ -0,0 +1,31 @@
+# Window size
+width, height = 1200, 800
+
+# Stylings
+style_sheet = """
+    QLabel {
+        font-size: 18px;
+    }
+    QPushButton {
+        font-size: 18px;
+        min-height: 25px;
+    }
+    QLineEdit {
+        font-size: 18px;
+        height: 25px;
+    }
+    QCheckBox {
+        font-size: 18px;
+    }
+    QCheckBox::indicator {
+        width: 25px;
+        height: 25px;
+    }
+    QFileDialog {
+        height: 25px;
+    }
+    QComboBox {
+        font-size: 18px;
+        height: 25px;
+    }
+"""
