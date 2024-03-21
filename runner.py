6239409df45a8be78ef19d9454fdffe1bf35a9c1
Wei Jie Chew <weijie.chew@gmail.com>
2024-03-21 17:02:11 +0800

    Updated program organization and application name

--- runner.py
+++ runner.py
@@ -3,6 +3,8 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
+app.setOrganizationName('Chew Wei Jie')
+app.setApplicationName('Monthly Sales Report Generator')
 excel_form = ExcelForm()
 excel_form.show()
 sys.exit(app.exec_())
