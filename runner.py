56c0fcc8646d581a8f7906a3bda56b8b13d9e001
Wei Jie Chew <weijie.chew@gmail.com>
2024-03-28 13:41:58 +0800

    Added main function to runner.py

--- runner.py
+++ runner.py
@@ -2,9 +2,13 @@ from excel_form import ExcelForm
 import sys
 from PyQt5.QtWidgets import QApplication
 
-app = QApplication(sys.argv)
-app.setOrganizationName('Scloud')
-app.setApplicationName('Monthly Sales Report Generator')
-excel_form = ExcelForm()
-excel_form.show()
-sys.exit(app.exec_())
+def main():
+    app = QApplication(sys.argv)
+    app.setOrganizationName('Scloud')
+    app.setApplicationName('Monthly Sales Report Generator')
+    excel_form = ExcelForm()
+    excel_form.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
